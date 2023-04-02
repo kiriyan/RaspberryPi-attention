7021cc09ce79f1c8f50a716de5fdd0ac5c425f8f
SKgtsa <1298096916@qq.com>
2023-04-02 23:20:19 +0800

    修改文件结构 建立蓝牙与音频播放api

---
+++ utils/AudioPlayer.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+音频播放器组件
+"""
+
+
+def play(file_name, start_time=0):
+    """
+    播放函数
+    :param file_name: static/audio中文件名
+    :param start_time: 播放开始时间
+    :return:
+    """
+    pass
+
+
+def terminate():
+    """
+    终止播放函数
+    :return: 终止是否成功
+    """
+    return True
+
---
+++ utils/Bluetooth.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+蓝牙组件
+"""
+
+
+def getState():
+    """
+    获取蓝牙状态函数
+    :return: 蓝牙是否连接
+    """
+    return False
+
+
+def read():
+    """
+    读取特征值函数
+    :return: 当前的特征值
+    """
+    return ""
+
+
+def send(str):
+    """
+    发送字符串函数
+    :param str: 待发送字符串
+    :return: 发送是否成功
+    """
+    return True
