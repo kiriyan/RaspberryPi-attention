75e8f521d6ba37e3443e0a099c5d1c484a82a3c1
SKgtsa <1298096916@qq.com>
2023-04-03 11:58:20 +0800

    实现音频播放(未测试)

--- utils/AudioPlayer.py
+++ utils/AudioPlayer.py
@@ -5,14 +5,9 @@
 """
 
 
-def play(file_name, start_time=0):
-    """
-    播放函数
-    :param file_name: static/audio中文件名
-    :param start_time: 播放开始时间
-    :return:
-    """
-    pass
+from pydub import AudioSegment
+from pydub.playback import play
+from multiprocessing import Process
 
 
 def terminate():
@@ -20,5 +15,26 @@ def terminate():
     终止播放函数
     :return: 终止是否成功
     """
+    try:
+        AudioPlayer.audio_process.terminate()
+    except Exception as e:
+        return False
     return True
+
+
+def playAudio(file_name, start_time=0):
+    """
+    播放函数(仅支持wav格式)
+    :param file_name: static/audio中文件名
+    :param start_time: 播放开始时间(毫秒)
+    :return:
+    """
+    full_clip = AudioSegment.from_wav("../static/audio/" + file_name)
+    AudioPlayer.audio_process = Process(play, full_clip[start_time:])
+    AudioPlayer.audio_process.start()
+
+
+class AudioPlayer:
+
+    audio_process = Process()
 
